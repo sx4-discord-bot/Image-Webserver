f66ec027a9747b4c95e644fef79dc162f9025b47
Shea4 <sc4gaming@gmail.com>
2020-11-18 12:59:09 +0000

    Allows file upload for manipulate-colour

--- endpoints/manipulate-colour.py
+++ endpoints/manipulate-colour.py
@@ -1,32 +1,47 @@
 import colorsys
+from io import BytesIO
+from typing import Optional
 
-from handlers.handler import SingleImageHandler
+from PIL import Image
+
+from handlers.handler import Handler
 from utility.colour import as_rgb_tuple
-from utility.image import get_image_response, for_each_frame
+from utility.image import get_image_response, for_each_frame, get_image
 
 
-class TestHandler(SingleImageHandler):
+class ManipulateColourHandler(Handler):
 
     def __init__(self, app):
         super().__init__(app)
 
-        self.queries += [(["colour", "color"], str)]
+        self.methods = ["GET", "POST"]
 
-    def on_request(self, image):
+        self.aliases = ["manipulate-color"]
+        self.queries += [(["colour", "color"], int), (["image"], Optional[str])]
+
+    def on_request(self):
         colour = as_rgb_tuple(self.query("colour", int, 0) or self.query("color", int, 0))
+        image_url = self.query("image")
+
+        if image_url:
+            image = get_image(image_url)
+        else:
+            image = Image.open(BytesIO(self.request.get_data()))
 
         h, s, v = colorsys.rgb_to_hsv(colour[0] / 255, colour[1] / 255, colour[2] / 255)
 
         def parse(frame):
             frame = frame.convert("RGBA")
             pixels = frame.load()
-            for x in range(image.size[0]):
-                for y in range(image.size[1]):
+            for x in range(frame.size[0]):
+                for y in range(frame.size[1]):
                     r, g, b, a = pixels[x, y]
 
                     ch, cs, cv = colorsys.rgb_to_hsv(r / 255, b / 255, g / 255)
                     nr, ng, nb = colorsys.hsv_to_rgb(h, cs, cv)
 
                     pixels[x, y] = int(nr * 255), int(ng * 255), int(nb * 255), a
+
+            return frame
 
         return get_image_response(for_each_frame(image, parse), transparency=255)
