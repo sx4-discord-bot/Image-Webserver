1c7225b0693bce5b7878cbc617afb55815da8684
Shea4 <sc4gaming@gmail.com>
2020-11-26 12:28:37 +0000

    Small bug fix to avoid colour being None

--- endpoints/colour.py
+++ endpoints/colour.py
@@ -16,7 +16,7 @@ class ColourHandler(Handler):
         self.queries = [(["width", "w"], Optional[int]), (["height", "h"], Optional[int]), (["colour", "color"], int)]
 
     def on_request(self):
-        colour = self.query("colour", int) or self.query("color", int)
+        colour = self.query("colour", int, 0) or self.query("color", int, 0)
 
         width = self.query("width", int) or self.query("w", int) or 100
         height = self.query("height", int) or self.query("h", int) or 100
