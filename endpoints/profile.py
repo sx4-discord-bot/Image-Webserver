87df2ec39adc9676447e44e89f82dee4635b6e15
Shea4 <sc4gaming@gmail.com>
2021-06-13 12:18:01 +0100

    Allow profile to support gifs

--- endpoints/profile.py
+++ endpoints/profile.py
@@ -8,7 +8,8 @@ from PIL import Image, ImageDraw
 from handlers.handler import SingleImageHandler
 from utility.colour import as_rgb_tuple
 from utility.image import create_avatar, get_image_response, get_image_asset, get_font_optimal, get_font_asset, \
-    get_text_newlined, resize_to_ratio, draw_ellipse
+    get_text_newlined, resize_to_ratio, draw_ellipse, for_each_frame, extract_and_resize_to_ratio
+from utility.types import boolean
 
 
 class ProfileHandler(SingleImageHandler):
@@ -30,7 +31,8 @@ class ProfileHandler(SingleImageHandler):
             (["balance"], str),
             (["colour", "color"], int),
             (["banner_id"], Optional[str]),
-            (["directory"], str)
+            (["directory"], str),
+            (["gif"], Optional[bool])
         ]
 
     def on_request(self, avatar: Image):
@@ -40,71 +42,84 @@ class ProfileHandler(SingleImageHandler):
         height = self.body("height", int)
         married_users = self.body("married_users", list)[:5]
         balance = self.body("balance")
+        gif = self.body("gif", bool, False)
         banner_id = self.body("banner_id", str)
         directory = self.body("directory", str)
         reputation = self.body("reputation", int)
         colour_int = self.body("colour", int, 13997604) or self.body("color", int, 13997604)
         colour = as_rgb_tuple(colour_int)
 
-        if banner_id is None:
-            image = Image.new("RGBA", (935, 567), (41, 40, 39))
-        else:
-            with resize_to_ratio(Image.open(ProfileHandler.DIRECTORY.format(directory) + banner_id), (935, 567)) as image:
-                overlay = Image.new("RGBA", image.size, (41, 40, 39, 0))
-                draw = ImageDraw.Draw(overlay)
-                draw.rectangle((0, 567, 935, 0), (41, 40, 39, 216))
-
-                image = Image.alpha_composite(image, overlay)
-
         poppins_name = get_font_asset("poppins/Poppins-Medium.ttf", 42)
         poppins_title = get_font_asset("poppins/Poppins-Medium.ttf", 20)
         poppins_text = get_font_asset("poppins/Poppins-Medium.ttf", 23)
         poppins_rep = get_font_asset("poppins/Poppins-Medium.ttf", 32)
 
-        draw = ImageDraw.Draw(image)
+        def generate_profile(card):
+            draw = ImageDraw.Draw(card)
 
-        # Horizontal lines
-        draw.line((0, 11, 935, 11), colour, 24)
-        draw.line((0, 567 - 12, 935, 567 - 12), colour, 24)
+            # Horizontal lines
+            draw.line((0, 11, 935, 11), colour, 24)
+            draw.line((0, 567 - 12, 935, 567 - 12), colour, 24)
 
-        # Vertical lines
-        draw.line((11, 0, 11, 567), colour, 24)
-        draw.line((935 - 12, 0, 935 - 12, 567), colour, 24)
+            # Vertical lines
+            draw.line((11, 0, 11, 567), colour, 24)
+            draw.line((935 - 12, 0, 935 - 12, 567), colour, 24)
 
-        reputation_width, reputation_height = draw.textsize(str(reputation), poppins_rep)
-        total_width = 34 + reputation_width + 4
+            reputation_width, reputation_height = draw.textsize(str(reputation), poppins_rep)
+            total_width = 34 + reputation_width + 4
 
-        draw.rectangle((935 - 188, 0, 935, 63), colour)
-        draw.text(((935 - 202) + ((188 - total_width) / 2), 7), str(reputation), (255, 250, 245), poppins_rep)
-        draw.text(((935 - 198) + ((188 - total_width) / 2) + reputation_width, 6), "REP", (41, 40, 39), poppins_rep)
+            draw.rectangle((935 - 188, 0, 935, 63), colour)
+            draw.text(((935 - 202) + ((188 - total_width) / 2), 7), str(reputation), (255, 250, 245), poppins_rep)
+            draw.text(((935 - 198) + ((188 - total_width) / 2) + reputation_width, 6), "REP", (41, 40, 39), poppins_rep)
 
-        username, discrim = name.split("#")
-        discrim_width, discrim_height = draw.textsize("#" + discrim, poppins_name)
-        limited_username = get_text_newlined(username, poppins_name, 620 - discrim_width, 1)
+            username, discrim = name.split("#")
+            discrim_width, discrim_height = draw.textsize("#" + discrim, poppins_name)
+            limited_username = get_text_newlined(username, poppins_name, 620 - discrim_width, 1)
 
-        draw.text((270, 78 - 20), "NAME", colour, poppins_title)
-        draw.text((268, 125 - 42), limited_username + "#" + discrim, (247, 246, 245), poppins_name)
+            draw.text((270, 78 - 20), "NAME", colour, poppins_title)
+            draw.text((268, 125 - 42), limited_username + "#" + discrim, (247, 246, 245), poppins_name)
 
-        draw.text((270, 172 - 20), "BIRTHDAY", colour, poppins_title)
-        draw.text((270, 204 - 23), birthday, (247, 246, 245), poppins_text)
+            draw.text((270, 172 - 20), "BIRTHDAY", colour, poppins_title)
+            draw.text((270, 204 - 23), birthday, (247, 246, 245), poppins_text)
 
-        draw.text((472, 172 - 20), "HEIGHT", colour, poppins_title)
-        draw.text((472, 202 - 23), f"{height}cm", (247, 246, 245), poppins_text)
+            draw.text((472, 172 - 20), "HEIGHT", colour, poppins_title)
+            draw.text((472, 202 - 23), f"{height}cm", (247, 246, 245), poppins_text)
 
-        draw.text((654, 169 - 20), "BALANCE", colour, poppins_title)
-        draw.text((654, 202 - 23), f"${balance}", (247, 246, 245), poppins_text)
+            draw.text((654, 169 - 20), "BALANCE", colour, poppins_title)
+            draw.text((654, 202 - 23), f"${balance}", (247, 246, 245), poppins_text)
 
-        draw.text((50, 293 - 20), "DESCRIPTION", colour, poppins_title)
-        draw.text((50, 320 - 23), get_text_newlined(description, poppins_text, 500, 7), (247, 246, 245), poppins_text)
+            draw.text((50, 293 - 20), "DESCRIPTION", colour, poppins_title)
+            draw.text((50, 320 - 23), get_text_newlined(description, poppins_text, 500, 7), (247, 246, 245),
+                      poppins_text)
 
-        draw.text((652, 293 - 20), "PARTNERS", colour, poppins_title)
-        draw.text((652, 320 - 23), "\n".join([get_text_newlined(user.split("#")[0], poppins_text, 250, 1) for user in married_users]) if len(married_users) > 0 else "No one :(", (247, 246, 245), poppins_text)
+            draw.text((652, 293 - 20), "PARTNERS", colour, poppins_title)
+            draw.text((652, 320 - 23), "\n".join(
+                [get_text_newlined(user, poppins_text, 250, 1) for user in married_users]) if len(
+                married_users) > 0 else "No one :(", (247, 246, 245), poppins_text)
 
-        draw_ellipse(image, (31, 31, 245, 245), 1, (255, 255, 255), 4)
+            draw_ellipse(card, (31, 31, 245, 245), 1, (255, 255, 255), 4)
 
-        image.paste(avatar, (34, 34), avatar)
+            card.paste(avatar, (34, 34), avatar)
 
-        return get_image_response([image])
+            return card
+
+        if banner_id is None:
+            image = Image.new("RGBA", (935, 567), (41, 40, 39))
+            return get_image_response([generate_profile(image)])
+        else:
+            with Image.open(ProfileHandler.DIRECTORY.format(directory) + banner_id) as image:
+                def parse(frame):
+                    frame = resize_to_ratio(frame.convert("RGBA"), (935, 567))
+
+                    overlay = Image.new("RGBA", frame.size, (41, 40, 39, 0))
+                    draw = ImageDraw.Draw(overlay)
+                    draw.rectangle((0, 567, 935, 0), (41, 40, 39, 216))
+
+                    frame = Image.alpha_composite(frame, overlay)
+
+                    return generate_profile(frame)
+
+                return get_image_response(for_each_frame(image, parse) if gif else [parse(image)])
 
     def old(self, avatar):
         name = self.body("name")
