e73c08368ff2da0e4caad26dac21cfb2dd5613dd
Shea4 <sc4gaming@gmail.com>
2021-06-24 14:17:03 +0100

    Add median colour endpoint

--- endpoints/median-colour.py
+++ endpoints/median-colour.py
@@ -7,13 +7,13 @@ from handlers.handler import SingleImageHandler
 from utility.colour import as_rgb
 
 
-class AverageColourHandler(SingleImageHandler):
+class MedianColourHandler(SingleImageHandler):
 
     def __init__(self, app):
         super().__init__(app)
 
-        self.aliases = ["average-color"]
+        self.aliases = ["median-color"]
 
     def on_request(self, image):
         stat = ImageStat.Stat(image.convert("RGB"))
-        return Response(status=200, response=json.dumps({"status": 200, "colour": as_rgb(tuple(int(x) for x in stat.average))}), content_type="application/json")
+        return Response(status=200, response=json.dumps({"status": 200, "colour": as_rgb(tuple(int(x) for x in stat.median))}), content_type="application/json")
