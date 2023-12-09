faec742eb3ce02ed3a4a3be28a9071b464fe4f8b
Shea4 <sc4gaming@gmail.com>
2023-12-09 20:13:43 +0000

    Makes the accent_colour white or black unless otherwise specified

--- handlers/handler.py
+++ handlers/handler.py
@@ -137,8 +137,11 @@ class GraphHandler(Handler):
     @check_queries
     def __call__(self):
         self.background_colour = as_rgb_tuple(self.body("background_colour", int, 0x121212))
-        self.surface_colour = (int(255 - colorsys.rgb_to_hls(*self.background_colour)[1]),) * 3
-        self.accent_colour = as_rgb_tuple(self.body("accent_colour", int, as_rgb(self.surface_colour)))
+
+        brightness = int(255 - colorsys.rgb_to_hls(*self.background_colour)[1])
+
+        self.surface_colour = (brightness,) * 3
+        self.accent_colour = as_rgb_tuple(self.body("accent_colour", int, as_rgb((255 if brightness > 127 else 0,) * 3)))
         self.antialias = min(5, max(1, self.body("antialias", int, 3)))
 
         return self.on_request()
